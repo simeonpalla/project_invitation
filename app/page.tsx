9e61023d119d7b7347951ab45632d5a44b73552c
Simeon Palla <simeonpalla@gmail.com>
2026-03-04 16:17:16 +0530

    App initial install

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-serif tracking-widest text-gold-500">
+        Invitation SaaS Engine
+      </h1>
+    </main>
+  );
+}
